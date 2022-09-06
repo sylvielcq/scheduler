29ba8ca3248f7beeaa66e8dcd1450e94cf5ff063
sylvielcq <slcq.dev@gmail.com>
2022-09-05 21:50:25 -0700

    Create variable button text in Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,9 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   return (
+    <button>
+      {props.children}
+    </button>
+   );
 }
