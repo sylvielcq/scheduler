656e1e343a2be4893c32b0589c1230125c2f5841
sylvielcq <slcq.dev@gmail.com>
2022-09-07 14:45:56 -0700

    Refactor DayListItem and DayList for consistency

--- stories/index.js
+++ stories/index.js
@@ -36,7 +36,7 @@ storiesOf("DayListItem", module)
   .add("Selected", () => <DayListItem name="Monday" spots={5} selected />) 
   .add("Full", () => <DayListItem name="Monday" spots={0} />)
   .add("Clickable", () => (
-    <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} />
+    <DayListItem name="Tuesday" setDay={() => action("setDay")("Tuesday")} spots={5} />
   ));
 
 
