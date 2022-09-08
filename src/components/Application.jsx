c20e3d353d3e6334c46c680d1168a56d10fedc5e
sylvielcq <slcq.dev@gmail.com>
2022-09-08 13:40:13 -0700

    Add getAppointmentsForDay selector function

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,25 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import "components/Application.scss";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   "1": {
@@ -63,6 +47,7 @@ const appointments = {
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
 
   const appointmentsList = Object.values(appointments).map(appointment => {
     return (
@@ -72,6 +57,12 @@ export default function Application(props) {
       />
     )
   });
+
+  useEffect(() => {
+    axios.get('/api/days').then(response => {
+      setDays([...response.data]);
+    });
+  }, []);
 
   return (
     <main className="layout">
---
+++ src/helpers/selectors.jsx
@@ -0,0 +1,21 @@
+export function getAppointmentsForDay(state, day) {
+  const filteredDay = state.days.filter(dayItem => dayItem.name === day); // returns an array
+
+  // If there is no filteredDay
+  if (filteredDay.length === 0) {
+    return [];
+  }
+
+  // filteredDay[0] as we want to look into the object inside of the filteredDay array
+  const filteredAppointmentsID = filteredDay[0].appointments;
+
+  // declaring a filteredAppointments array which will hold the day's appointments
+  const filteredAppointments = [];
+
+
+  filteredAppointmentsID.map(id => {
+    filteredAppointments.push(state.appointments[id]);
+  });
+  
+  return filteredAppointments;
+};
