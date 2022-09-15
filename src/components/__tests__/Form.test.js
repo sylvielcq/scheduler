689e02806ca1a7680011048ee6ba8fe6eac7d9a0
sylvielcq <slcq.dev@gmail.com>
2022-09-14 20:04:33 -0700

    Fix form test

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { render, cleanup, fireEvent } from "@testing-library/react";
 
-import Form from "components/Appointment/form";
+import Form from "components/Appointment/Form";
 
 afterEach(cleanup);
 
